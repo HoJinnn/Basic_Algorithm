f0d893b5f9897994eb23f8d1b9cc2a82daef828e
HoJinLee <105032621+HoJinnn@users.noreply.github.com>
2022-09-16 21:51:13 +0900

    Create 괄호문자제거.java
    stack의 메서드인 pop()은 인자를 꺼낸 후에 리턴한다. 그래서 while(answer.pop() != '('); 가 통한다.
    stack은 인자를 꺼내올때는 get()을 활용해서 가져온다. size() 함수도 있음.

---
+++ 호진/괄호문자제거.java
@@ -0,0 +1,39 @@
+2. 괄호문자제거
+
+설명
+입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+
+입력
+첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+
+출력
+남은 문자만 출력한다.
+
+예시 입력 1 
+(A(BC)D)EF(G(H)(IJ)K)LM(N)
+  
+예시 출력 1
+EFLM
+
+import java.util.*;
+  
+public class Main {
+  public Stack<Character> solution(String str){
+    Stack<Character> answer = new Stack<>();
+    for(char x : str.toCharArray()){
+      if(x == ')'){
+        while(answer.pop() != '(');
+      }else{
+        answer.push(x); 
+      }
+    }
+    return answer;
+  }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    for(char x : T.solution(str))
+      System.out.print(x);
+  }
+}
